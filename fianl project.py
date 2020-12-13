66d3e52f51d46e23a4f3d9057db181cb1f6fb0d8
yangyuhsiang <75618907+yangyuhsiang@users.noreply.github.com>
2020-12-14 00:14:43 +0800

    把石頭的圖像放到臉上了
    換了一個新的寫法，下一步就是要把剪刀跟布隨機替換

--- fianl project.py
+++ fianl project.py
@@ -5,57 +5,48 @@ import random
 from math import hypot
 
 
-# 讀取剪刀石頭布的圖檔
-stone_pic = cv.imread("C:\\Users\\user\\Desktop\\project\\PBC--final-project\\rock.png", cv.IMREAD_UNCHANGED)
-predictor_dat = dlib.shape_predictor("C:\\Users\\user\\Desktop\\project\\PBC--final-project\\shape_predictor_68_face_landmarks.dat")
+stone = cv.imread('C:\\Users\\user\\Desktop\\rock.png', cv.IMREAD_UNCHANGED)
+capture = cv.VideoCapture(0, cv.CAP_DSHOW)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("C:\\Users\\user\\Desktop\\project\\PBC--final-project\\shape_predictor_68_face_landmarks.dat")
 
+while True:
+    ret, image = capture.read()
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    faces = detector(gray_image, 0)
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()            
 
-def paper_scissor_stone(stone, predictor):
-    capture = cv.VideoCapture(0, cv.CAP_DSHOW)
-    detector = dlib.get_frontal_face_detector()
-    while True:
-        ret, image = capture.read()
-        gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        faces = detector(gray_image)
-        for face in faces:
-            landmarks = predictor(gray_image, face)
-
-            # 算你的圖要多大(先用鼻子的大小去算)
-            nose_top = (landmarks.part(27).x, landmarks.part(27).y)
-            nose_bottom = (landmarks.part(33).x, landmarks.part(33).y)
-
-            pic_high = int(hypot((nose_top[0] - nose_bottom[0]),(nose_top[1] - nose_bottom[1])))
-            '''因為每張圖的形狀不同，所以用圖片的比例去做調整'''
-            stone_width = int(pic_high * 1.17)
-
-            # 要替換的區域
-            stone_bottom = (int(nose_top[0]), int(nose_top[1]))
-            stone_top = (int(nose_top[0]), int(nose_top[1] - pic_high))
-            stone_left_under_corner = (int(stone_bottom[0]-stone_width/2), int(stone_bottom[1]))
-            stone_right_top_corner = (int(stone_top[0]+stone_width/2), int(stone_top[1]))
-
-
-            # 重新製作石頭的大小
-            stone = cv.resize(stone, (pic_high, stone_width))
-            stone_gray = cv.cvtColor(stone, cv.COLOR_BGR2GRAY)
-            _, stone_mask = cv.threshold(stone_gray, 35, 255, cv.THRESH_BINARY_INV)
-
-            '''不知道為什麼不能局部顯示要放石頭的區域'''
-            stone_area = image[stone_left_under_corner[1]:stone_right_top_corner[1],
-                               stone_left_under_corner[0]:stone_right_top_corner[0]]
+        width = int(x2 - x1)
+        high = int(y2 - y1)
             
-            face_no_stone = cv.bitwise_and(stone_area, stone_area, mask = stone_mask)
-
-            final_face = cv.add(face_no_stone, resize_stone)
-
-            image[stone_left_under_corner[1]:stone_right_top_corner[1],
-                  stone_left_under_corner[0]:stone_right_top_corner[0]] = final_face
+        w1 = width//2
+        h1 = high//2
             
+        w2 = width//4
+        h2 = high//4
 
+        resize_stone = cv.resize(stone, (w1, h1))
+        stone_mask_bgr = resize_stone[:, :, :3]
+        stone_alpha_ch = resize_stone[:, :, 3]
+        _, stone_mask = cv.threshold(stone_alpha_ch, 220, 255, cv.THRESH_BINARY)
+
+
+            
+        stone_area_no_face = cv.bitwise_not(stone_mask)
+        stone_area = image[y1-h2:y1+h1-h2, x1+w2:x1+w1+w2]
+
+        stone_part = cv.bitwise_and(stone_mask_bgr, stone_mask_bgr, mask=stone_mask)
+        face_part = cv.bitwise_and(stone_area, stone_area, mask=stone_area_no_face)
+            
+        final_stone = cv.add(face_part, stone_part)
+        image[y1-h2:y1+h1-h2, x1+w2:x1+w1+w2] = final_stone
+            
         image = cv.flip(image, 1)
-        cv.imshow('image', stone_area)
-        key = cv.waitKey(50)
-        if key == 27:
-            break
-
-paper_scissor_stone(stone_pic, predictor_dat)
+        cv.imshow('stone_mask', image)
+    key = cv.waitKey(50)
+    if key == 27:
+        break
