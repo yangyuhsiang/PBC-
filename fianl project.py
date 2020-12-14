6b4ecb2418ab383a4915aec7cde65e741318f23e
yangyuhsiang <75618907+yangyuhsiang@users.noreply.github.com>
2020-12-14 21:11:48 +0800

    加了一些註解

--- fianl project.py
+++ fianl project.py
@@ -12,6 +12,7 @@ capture = cv.VideoCapture(0, cv.CAP_DSHOW)
 detector = dlib.get_frontal_face_detector()
 
 
+# stone 的大小調整，還有遮罩的函數
 def stone_change(stone, w1, h1):
     resize_stone = cv.resize(stone, (w1, h1))
     stone_mask_bgr = resize_stone[:, :, :3]
@@ -21,6 +22,7 @@ def stone_change(stone, w1, h1):
     return pic_mask, pic_part
 
 
+# paper的大小調整函數，還有遮罩的函數
 def paper_change(paper, w1, h1):
     resize_paper = cv.resize(paper, (w1, h1))
     paper_mask_bgr = resize_paper[:, :, :3]
@@ -30,6 +32,7 @@ def paper_change(paper, w1, h1):
     return pic_mask, pic_part
 
 
+# 剪刀的大小調整函數，還有遮罩的函數
 def scissor_change(scissor, w1, h1):
     resize_scissor = cv.resize(scissor, (w1, h1))
     scissor_mask_bgr = resize_scissor[:, :, :3]
@@ -44,11 +47,13 @@ while True:
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     faces = detector(gray_image, 0)
     for face in faces:
+        # 找到臉的座標
         x1 = face.left()
         y1 = face.top()
         x2 = face.right()
         y2 = face.bottom()            
 
+        # 定義剪刀石頭布的高和寬
         face_width = int(x2 - x1)
         face_hight = int(y2 - y1)
             
@@ -67,17 +72,18 @@ while True:
         else:
             pic_mask, pic_part = scissor_change(scissor, pic_width1, pic_hight1)
 
+        # 額頭要放上剪刀石頭布的位置的處理
         image_area_no_face = cv.bitwise_not(pic_mask)
         pic_area = image[y1-pic_hight2 : y1+pic_hight1-pic_hight2,
                            x1+pic_width2 : x1+pic_width1+pic_width2]
 
-
         face_part = cv.bitwise_and(pic_area, pic_area, mask=image_area_no_face)
-
         final_part = cv.add(face_part, pic_part)
+
         image[y1-pic_hight2 : y1+pic_hight1-pic_hight2,
               x1+pic_width2 : x1+pic_width1+pic_width2] = final_part
-    
+
+    # 最後要show出的image的處理，然後show出
     image = cv.flip(image, 1)
     cv.imshow('final', image)
     key = cv.waitKey(50)
