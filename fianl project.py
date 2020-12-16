103048d6d86b13009a7f60e5be5bbf3051a22359
yangyuhsiang <75618907+yangyuhsiang@users.noreply.github.com>
2020-12-17 00:02:59 +0800

    寫完偵測不到人臉還有臉跑出範圍時的情況了
    再來是要寫平手的情況

--- fianl project.py
+++ fianl project.py
@@ -3,7 +3,6 @@ import numpy as np
 import dlib
 import random
 from math import hypot
-import sys
 
 stone = cv.imread('C:\\Users\\user\\Desktop\\rock.png', cv.IMREAD_UNCHANGED)
 paper = cv.imread('C:\\Users\\user\\Desktop\\paper.png', cv.IMREAD_UNCHANGED)
@@ -42,89 +41,106 @@ def scissor_change(scissor, w1, h1):
     pic_part = cv.bitwise_and(scissor_mask_bgr, scissor_mask_bgr, mask=pic_mask)
     return pic_mask, pic_part
 
-Round = 0  # 紀錄是第幾張臉的變數
-while True:
-    ret, image = capture.read()
-    image = cv.flip(image, 1)
-    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    faces = detector(gray_image, 0)
-    for face in faces:
-        # 找到臉的座標
-        x1 = face.left()
-        y1 = face.top()
-        x2 = face.right()
-        y2 = face.bottom()            
+# 剪刀石頭布的function
+def paper_scissor_stone():
+    Round = 0  # 紀錄是第幾張臉的變數
+    while True:
+        ret, image = capture.read()
+        image_hight = image.shape[0]
+        image_width = image.shape[1]
+        image = cv.flip(image, 1)
+        gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        faces = detector(gray_image, 0)
+        if len(faces) >= 1:
+            for face in faces:
+                # 找到臉的座標
+                x1 = face.left()
+                y1 = face.top()
+                x2 = face.right()
+                y2 = face.bottom()            
 
-        # 定義剪刀石頭布的高和寬
-        face_width = int(x2 - x1)
-        face_hight = int(y2 - y1)
+                # 定義剪刀石頭布的高和寬
+                face_width = int(x2 - x1)
+                face_hight = int(y2 - y1)
 
-        pic_width1 = face_width//2  # 剪刀石頭布的圖要resize的寬跟高
-        pic_hight1 = face_hight//2
+                pic_width1 = face_width//2  # 剪刀石頭布的圖要resize的寬跟高
+                pic_hight1 = face_hight//2
 
-        pic_width2 = face_width//4  # 剪刀石頭布的圖要放的位置
-        pic_hight2 = face_hight//4
+                pic_width2 = face_width//4  # 剪刀石頭布的圖要放的位置
+                pic_hight2 = face_hight//4
 
-        # 三個函數隨機變動，所以剪刀石頭布可以隨機換
-        type = random.randrange(0, 3)
-        # 第一張臉的紀錄
-        if Round == 0:
-            if type == 0:
-                pic_mask, pic_part = stone_change(stone, pic_width1, pic_hight1)
-                first_choice = [1, 0, 0]
-                first_face = face
+                # 如果要放圖的位置超過視窗，就不能繼續往下執行
+                pic_y1 = y1-pic_hight2
+                pic_y2 = y1+pic_hight1-pic_hight2
+                pic_x1 = x1+pic_width2
+                pic_x2 = x1+pic_width1+pic_width2
+                if pic_y2 <= image_width and pic_x2 <= image_hight and pic_x1 >= 0 and pic_y1 >= 0:
+                    # 三個函數隨機變動，所以剪刀石頭布可以隨機換
+                    type = random.randrange(0, 3)
+                    # 第一張臉的紀錄
+                    if Round == 0:
+                        if type == 0:
+                            pic_mask, pic_part = stone_change(stone, pic_width1, pic_hight1)
+                            first_choice = [1, 0, 0]
+                            first_face = face
 
-            elif type == 1:
-                pic_mask, pic_part = paper_change(paper, pic_width1, pic_hight1)
-                first_choice = [0, 1, 0]
-                first_face = face
+                        elif type == 1:
+                            pic_mask, pic_part = paper_change(paper, pic_width1, pic_hight1)
+                            first_choice = [0, 1, 0]
+                            first_face = face
 
-            elif type == 2:
-                pic_mask, pic_part = scissor_change(scissor, pic_width1, pic_hight1)
-                first_choice = [0, 0, 1]
-                first_face = face
+                        elif type == 2:
+                            pic_mask, pic_part = scissor_change(scissor, pic_width1, pic_hight1)
+                            first_choice = [0, 0, 1]
+                            first_face = face
 
-            Round = 1
-            # 第一張臉的出拳紀錄完成，下一輪要記錄第二張臉的出拳
+                        Round = 1
+                        # 第一張臉的出拳紀錄完成，下一輪要記錄第二張臉的出拳
 
-        # 第二張臉的紀錄
-        else:
-            if type == 0:
-                pic_mask, pic_part = paper_change(stone, pic_width1, pic_hight1)
-                second_choice = [1, 0, 0]
-                second_face = face
+                    # 第二張臉的紀錄
+                    else:
+                        if type == 0:
+                            pic_mask, pic_part = paper_change(stone, pic_width1, pic_hight1)
+                            second_choice = [1, 0, 0]
+                            second_face = face
 
-            elif type == 1:
-                pic_mask, pic_part = paper_change(paper, pic_width1, pic_hight1)
-                second_choice = [0, 1, 0]
-                second_face = face
+                        elif type == 1:
+                            pic_mask, pic_part = paper_change(paper, pic_width1, pic_hight1)
+                            second_choice = [0, 1, 0]
+                            second_face = face
 
-            elif type == 2:
-                pic_mask, pic_part = paper_change(scissor, pic_width1, pic_hight1)
-                second_choice = [0, 0, 1]
-                second_face = face
-            Round = 0
-            # 第二張臉的紀錄完成，可以重新記錄了
+                        elif type == 2:
+                            pic_mask, pic_part = paper_change(scissor, pic_width1, pic_hight1)
+                            second_choice = [0, 0, 1]
+                            second_face = face
+                        Round = 0
+                        # 第二張臉的紀錄完成，可以重新記錄了
 
-        # 額頭要放上剪刀石頭布的位置的處理
-        image_area_no_face = cv.bitwise_not(pic_mask)
-        pic_area = image[y1-pic_hight2 : y1+pic_hight1-pic_hight2,
-                         x1+pic_width2 : x1+pic_width1+pic_width2]
+                    # 額頭要放上剪刀石頭布的位置的處理
+                    image_area_no_face = cv.bitwise_not(pic_mask)
+                    pic_area = image[pic_y1 : pic_y2, pic_x1 : pic_x2]
 
-        face_part = cv.bitwise_and(pic_area, pic_area, mask=image_area_no_face)  # 有問題喔 因為臉超出範圍了
-        final_part = cv.add(face_part, pic_part)
+                    face_part = cv.bitwise_and(pic_area, pic_area, mask=image_area_no_face)  # 有問題喔 因為臉超出範圍了
+                    final_part = cv.add(face_part, pic_part)
 
-        image[y1-pic_hight2 : y1+pic_hight1-pic_hight2,
-              x1+pic_width2 : x1+pic_width1+pic_width2] = final_part
+                    image[pic_y1 : pic_y2, pic_x1 : pic_x2] = final_part
+                else:  # 臉超出範圍的時候
+                    pass
 
-    # 最後要show出的image的處理，然後show出
-    cv.imshow('final', image)
-    if cv.waitKey(1) &  0xFF == ord('q'):
-        if len(faces) == 2:  # 如果有偵測到兩張臉才可以停下
-            record_image = image
-            capture.release()
-            cv.destroyAllWindows()
-            break
+        else:  # 沒有任何人臉的時候
+            pass
+
+            # 最後要show出的image的處理，然後show出
+        cv.imshow('final', image)
+        if cv.waitKey(1) &  0xFF == ord('q'):
+            if len(faces) == 2:  # 如果有偵測到兩張臉才可以停下
+                record_image = image
+                capture.release()
+                cv.destroyAllWindows()
+                break
+    return record_image, first_choice, second_choice, first_face, second_face
+
+record_image, first_choice, second_choice,  first_face, second_face = paper_scissor_stone()
 
 
 # 判斷誰輸誰贏的function
@@ -133,26 +149,29 @@ def who_win(first_choice, second_choice):
     second_person = second_choice.index(1)
 
     # 一開始先預設second person win
-    who_win = 2
+    result = 1
     win_face = second_face
     loser_face = first_face
 
     # 平手
     if((first_person == 0 and second_person == 0) or (first_person == 1 and second_person == 1)
         or (first_person == 2 and second_person == 2)):
-        who_win = 0
-        print('平手')
-        sys.exit()
+        result = 0
 
     # first_person 贏
     elif((first_person == 0 and second_person == 2) or (first_person == 1 and second_person == 0)
         or(first_person == 2 and second_person == 1)):
-        who_win = 1
+        result = 1
         win_face = first_face
         loser_face = second_face
-    return win_face, loser_face
+    return win_face, loser_face, result
 
-win_face, loser_face = who_win(first_choice, second_choice)
+win_face, loser_face, result = who_win(first_choice, second_choice)
+
+'''
+# 處理平手的情況
+def tie():
+'''
 
 # 處理輸家的特效
 def loser(record_image, loser_face):
@@ -190,7 +209,9 @@ def loser(record_image, loser_face):
 
     return record_image
 
-loser(record_image, loser_face)
+# 如果有輸有贏的話，那就印出輸贏的結果
+if result == 1:
+    loser(record_image, loser_face)
 
 # show出結果
 cv.imshow('record_image',record_image)
