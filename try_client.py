b4a9dca0ec5a8dbc0e41f7454abe23da6505908b
maxleeNametaken <75820272+maxleeNametaken@users.noreply.github.com>
2020-12-23 12:09:51 +0800

    Add files via upload
    Modified server and client in waiting for further integration.

--- try_client.py
+++ try_client.py
@@ -3,26 +3,29 @@ import socket
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.26"
+# Change to local ipv4 ip address before execution
+SERVER = "10.45.251.147"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+print(client.recv(2048).decode(FORMAT))
 
 def send(msg):
     message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
+    # msg_length = len(message)
+    # send_length = str(msg_length).encode(FORMAT)
+    # send_length += b' ' * (HEADER - len(send_length))
+    # client.send(send_length)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
 
-send("Hello World!")
-input()
-send("Hello Everyone!")
-input()
-send("Hello Tim!")
 
-send(DISCONNECT_MESSAGE)
+while True:
+    message = input()
+    if message == "Disconnect" or message == "N":
+        client.shutdown()
+        client.close()
+        break
+    else:
+        send(message)
+        print(client.recv(2048).decode(FORMAT))
--- try_server.py
+++ try_server.py
@@ -11,33 +11,98 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-
-    conn.close()
-
-
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+# def handle_client(conn, addr):
+#     print(f"[NEW CONNECTION] {addr} connected.")
+#
+#     connected = True
+#     while connected:
+#         msg_length = conn.recv(HEADER).decode(FORMAT)
+#         if msg_length:
+#             msg_length = int(msg_length)
+#             msg = conn.recv(msg_length).decode(FORMAT)
+#             if msg == DISCONNECT_MESSAGE:
+#                 connected = False
+#
+#             print(f"[{addr}] {msg}")
+#             conn.send("Msg received".encode(FORMAT))
+#
+#     conn.close()
+#
+#
+# def start():
+#     server.listen()
+#     print(f"[LISTENING] Server is listening on {SERVER}")
+#     while True:
+#         conn, addr = server.accept()
+#         thread = threading.Thread(target=handle_client, args=(conn, addr))
+#         thread.start()
+#         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
 print("[STARTING] server is starting...")
-start()
+server.listen(2)
+print(f'Server is listening on {SERVER}')
+conn1, addr1 = server.accept()
+print('Connected to:', addr1)
+conn1.send("You're Connected!".encode())
+conn2, addr2 = server.accept()
+print('Connected to:', addr2)
+conn2.send("You're Connected!".encode())
+
+while True:
+    # msg_length1 = conn1.recv(HEADER).decode(FORMAT)
+    # if msg_length1:
+    #     msg_length1 = int(msg_length1)
+    ans1 = conn1.recv(2048).decode(FORMAT)
+
+    # msg_length2 = conn2.recv(HEADER).decode(FORMAT)
+    # if msg_length2:
+    #     msg_length2 = int(msg_length2)
+    ans2 = conn2.recv(2048).decode(FORMAT)
+
+    winner = -1
+    ans1 = ans1.strip()
+    ans2 = ans2.strip()
+
+    print(ans1)
+    print(ans2)
+
+    if ans1 != 'R' and ans1 != 'P' and ans1 != 'S':
+        print('Value Error')
+        conn1.send('Bad Value'.encode())
+    elif ans2 != 'R' and ans2 != 'P' and ans2 != 'S':
+        print('Value Error')
+        conn2.send('Bad Value'.encode())
+    else:
+        if ans1 == "R" and ans2 == "S":
+            winner = 0
+        elif ans1 == "S" and ans2 == "R":
+            winner = 1
+        elif ans1 == "P" and ans2 == "R":
+            winner = 0
+        elif ans1 == "R" and ans2 == "P":
+            winner = 1
+        elif ans1 == "S" and ans2 == "P":
+            winner = 0
+        elif ans1 == "P" and ans2 == "S":
+            winner = 1
+    if winner == -1:
+        conn1.send('Draw. Reset Game? Y/N'.encode())
+        conn2.send('Draw. Reset Game? Y/N'.encode())
+    elif winner == 0:
+        conn1.send('You WON!!! Reset Game? Y/N'.encode())
+        conn2.send('You LOSE!!! Reset Game? Y/N'.encode())
+    elif winner == 1:
+        conn1.send('You LOSE!!! Reset Game? Y/N'.encode())
+        conn2.send('You WON!!! Reset Game? Y/N'.encode())
+
+    res1 = conn1.recv(2048).decode(FORMAT)
+    res2 = conn2.recv(2048).decode(FORMAT)
+    print(res1)
+    print(res2)
+    if res1 == 'Y' and res2 == 'Y':
+        conn1.send("Restart!".encode())
+        conn2.send("Restart!".encode())
+        continue
+    else:
+        break
