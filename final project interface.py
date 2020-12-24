4b317eb260ee056c9627087261d4cbafbaa8733c
ian-su-0110 <b09302264@ntu.edu.tw>
2020-12-24 21:44:39 +0800

    Update final project interface.py

--- final project interface.py
+++ final project interface.py
@@ -28,9 +28,9 @@ print(player_num)
 
 
 # 按下確認connected後才開始進行
-stone = cv.imread('C:\\Users\\user\\Desktop\\rock.png', cv.IMREAD_UNCHANGED)
-paper = cv.imread('C:\\Users\\user\\Desktop\\paper.png', cv.IMREAD_UNCHANGED)
-scissor = cv.imread('C:\\Users\\user\\Desktop\\scissor.png', cv.IMREAD_UNCHANGED)
+stone = cv.imread("C:\\Users\\Ian Su\\Desktop\\PBC--final-project\\rock.png", cv.IMREAD_UNCHANGED)
+paper = cv.imread("C:\\Users\\Ian Su\\Desktop\\PBC--final-project\\paper.png", cv.IMREAD_UNCHANGED)
+scissor = cv.imread("C:\\Users\\Ian Su\\Desktop\\PBC--final-project\\scissor.png", cv.IMREAD_UNCHANGED)
 capture = cv.VideoCapture(0, cv.CAP_DSHOW)
 detector = dlib.get_frontal_face_detector()
 
@@ -141,22 +141,21 @@ def video_stream():
     main_inter.lblMain.after(1, video_stream)
 
 
-class MainInterface(tk.Frame):
+class MainInterfacePlayer1(tk.Frame):
     
     
     def __init__(self):
         tk.Frame.__init__(self)
         self.grid()
         self.createWidgets()
-
-
+    
+    
     def createWidgets(self):
         f1 = tkFont.Font(size=16, family='Microsoft JhengHei')
         f2 = tkFont.Font(size=16, family='Courier New')
         # 主視窗以及右方回合、勝、負顯示
         self.lblMain = tk.Label(self)
-        ##
-        ##
+
         self.lblRound = tk.Label(self, text='回合', height=1, width=4, font=f1)
         self.lblShowRound = tk.Label(self, text='4', height=1, width=4, font=f2)
         self.lblWin = tk.Label(self, text='勝', height=1, width=4, font=f1)
@@ -174,6 +173,67 @@ class MainInterface(tk.Frame):
         self.rb3_2 = tk.Radiobutton(self, text='三戰兩勝', variable=self.mode, value='三戰兩勝', command=self.print_selection, font=f1)
         
         self.btnSend = tk.Button(self, text='確認', height=1, width=4, command=self.send_request, font=f1)
+        
+        
+        # 遊戲說明按鈕,點一下會跳出出拳說明
+        self.btnIns = tk.Button(self, text='遊戲\n說明', height=2, width=4, command=self.instruction, font=f1)
+        
+        
+        # 排版
+        self.lblMain.grid(row=2, rowspan=6, column=0, columnspan=6, sticky=tk.NE+tk.SW)
+        self.lblRound.grid(row=2, column=6, sticky=tk.NE+tk.SW)
+        self.lblShowRound.grid(row=3, column=6, sticky=tk.NE+tk.SW)
+        self.lblWin.grid(row=4, column=6, sticky=tk.NE+tk.SW)
+        self.lblShowWin.grid(row=5, column=6, sticky=tk.NE+tk.SW)
+        self.lblLose.grid(row=6, column=6, sticky=tk.NE+tk.SW)
+        self.lblShowLose.grid(row=7, column=6, sticky=tk.NE+tk.SW)
+        
+        self.lblMode.grid(row=0, rowspan=2, column=0, sticky=tk.NE+tk.SW)
+        self.rb7_4.grid(row=0, rowspan=2, column=1)
+        self.rb5_3.grid(row=0, rowspan=2, column=2)
+        self.rb3_2.grid(row=0, rowspan=2, column=3)
+        self.btnSend.grid(row=0, rowspan=2, column=4)
+        
+        self.btnIns.grid(row=0, rowspan=2, column=6)
+    
+    
+    def print_selection(self):
+        self.lblMode.config(text='您已選擇 ' + self.mode.get())
+        
+    
+    def send_request(self):
+        if self.mode.get() == '七戰四勝':
+            client.send('2'.encode())
+        elif self.mode.get() == '五戰三勝':
+            client.send('1'.encode())
+        else:
+            client.send('0'.encode())
+            
+        
+    def instruction(self):  # 這裡放出拳的說明
+        tkinter.messagebox.showinfo(title='遊戲說明', message='如果你希望出剪刀：剪刀剪刀剪刀\n如果你希望出石頭：石頭石頭石頭\n如果你希望出布：布布布')
+
+
+class MainInterfacePlayer2(tk.Frame):
+    
+    
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.createWidgets()
+    
+    
+    def createWidgets(self):
+        f1 = tkFont.Font(size=16, family='Microsoft JhengHei')
+        f2 = tkFont.Font(size=16, family='Courier New')
+        # 主視窗以及右方回合、勝、負顯示
+        self.lblMain = tk.Label(self)
+        self.lblRound = tk.Label(self, text='回合', height=1, width=4, font=f1)
+        self.lblShowRound = tk.Label(self, text='4', height=1, width=4, font=f2)
+        self.lblWin = tk.Label(self, text='勝', height=1, width=4, font=f1)
+        self.lblShowWin = tk.Label(self, text='3', height=1, width=4, font=f2)
+        self.lblLose = tk.Label(self, text='負', height=1, width=4, font=f1)
+        self.lblShowLose = tk.Label(self, text='1', height=1, width=4, font=f2)
         
         
         # 上方接受挑戰欄
@@ -198,33 +258,16 @@ class MainInterface(tk.Frame):
         self.lblShowWin.grid(row=5, column=6, sticky=tk.NE+tk.SW)
         self.lblLose.grid(row=6, column=6, sticky=tk.NE+tk.SW)
         self.lblShowLose.grid(row=7, column=6, sticky=tk.NE+tk.SW)
-        self.lblMode.grid(row=0, column=0, sticky=tk.NE+tk.SW)
-        self.rb7_4.grid(row=0, column=1)
-        self.rb5_3.grid(row=0, column=2)
-        self.rb3_2.grid(row=0, column=3)
-        self.btnSend.grid(row=0, column=4)
-        self.lblText1.grid(row=1, column=0, sticky=tk.NE+tk.SW)
-        self.lblCom.grid(row=1, column=1, sticky=tk.NE+tk.SW)
-        self.lblReMode.grid(row=1, column=2, sticky=tk.NE+tk.SW)
-        self.lblText2.grid(row=1, column=3, sticky=tk.NE+tk.SW)
-        self.btnY.grid(row=1, column=4)
-        self.btnN.grid(row=1, column=5)
+        
+        self.lblText1.grid(row=0, rowspan=2, column=0, sticky=tk.NE+tk.SW)
+        self.lblCom.grid(row=0, rowspan=2,column=1, sticky=tk.NE+tk.SW)
+        self.lblReMode.grid(row=0, rowspan=2, column=2, sticky=tk.NE+tk.SW)
+        self.lblText2.grid(row=0, rowspan=2, column=3, sticky=tk.NE+tk.SW)
+        self.btnY.grid(row=0, rowspan=2, column=4)
+        self.btnN.grid(row=0, rowspan=2, column=5)
         
         self.btnIns.grid(row=0, rowspan=2, column=6)
-    
-    
-    def print_selection(self):
-        self.lblMode.config(text='您已選擇 ' + self.mode.get())
-        
-    
-    def send_request(self):  # 未完成
-        if self.mode.get() == '七戰四勝':
-            client.send('2'.encode())
-        elif self.mode.get() == '五戰三勝':
-            client.send('1'.encode())
-        else:
-            client.send('0'.encode())
-
+ 
     
     def yes(self):  #未完成
         pass
